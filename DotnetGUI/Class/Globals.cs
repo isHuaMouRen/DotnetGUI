10769f7d82ce1012e51fc567bc24a9a369b67015
某人 华 <2825338723@qq.com>
2025-10-28 20:28:02 +0800

    [Publish] 发布Indev 1.0.0.0

--- DotnetGUI/Class/Globals.cs
+++ DotnetGUI/Class/Globals.cs
@@ -12,6 +12,7 @@ namespace DotnetGUI.Class
     {
         public static string? ExecutePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public static string? TempPath = Path.GetTempPath();
+        public static readonly string AppVersion = "Indev 1.0.0.0";
         public static string ConfigPath = $"{ExecutePath}\\config.json";
         public static JsonConfig.Config.Root? GlobanConfig = null;
 
