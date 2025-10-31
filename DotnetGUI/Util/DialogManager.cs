ed8ccd252b32a0b5039047df47b28b5dffce67df
isHuaMouRen <2825338723@qq.com>
2025-10-31 11:52:17 +0800

    [Change] 统一对话框显示方法

--- DotnetGUI/Util/DialogManager.cs
+++ DotnetGUI/Util/DialogManager.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ModernWpf.Controls;
+using System;
 using System.Threading.Tasks;
 
 namespace DotnetGUI.Util
 {
-    internal class DialogManager
+    public class DialogManager
     {
+        private static bool isDialogShow = false;
+
+        public static async Task ShowDialogAsync(ContentDialog dialog, Action primaryCallback, Action secondaryCallback, Action closeCallback)
+        {
+            // 等待直到没有对话框显示
+            await WaitForDialogToCloseAsync();
+
+            isDialogShow = true;
+            var result = await dialog.ShowAsync();
+            isDialogShow = false;
+
+            HandleDialogResult(result, primaryCallback, secondaryCallback, closeCallback);
+        }
+
+        private static async Task WaitForDialogToCloseAsync()
+        {
+            while (isDialogShow)
+            {
+                await Task.Delay(100);
+            }
+        }
+
+        private static void HandleDialogResult(ContentDialogResult result, Action primaryCallback, Action secondaryCallback, Action closeCallback)
+        {
+            if (result == ContentDialogResult.Primary)
+                primaryCallback();
+            else if (result == ContentDialogResult.Secondary)
+                secondaryCallback();
+            else
+                closeCallback();
+        }
     }
 }
