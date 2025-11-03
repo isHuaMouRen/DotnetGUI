cbb04aa031f7e49e5d512e3406361af87ac887ff
isHuaMouRen <2825338723@qq.com>
2025-11-03 12:42:52 +0800

    [Publish] 发布Indev 2.2.0.0

--- DotnetGUI/Class/Globals.cs
+++ DotnetGUI/Class/Globals.cs
@@ -15,7 +15,7 @@ namespace DotnetGUI.Class
         #region Var
         public static readonly string? ExecutePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public static readonly string? TempPath = Path.GetTempPath();
-        public static readonly string AppVersion = "Indev 2.1.0.0";
+        public static readonly string AppVersion = "Indev 2.2.0.0";
         public static readonly string ConfigPath = $"{ExecutePath}\\config.json";
         public static JsonConfig.Config.Root? GlobanConfig = null;
 
